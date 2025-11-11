b508d8b018609a97179e33fe978fdd78278ecbca
Leonardo Rodrigues <leonardorodrigues@MacBook-Air-de-Leonardo.local>
2025-11-11 20:15:24 -0300

    feat(loja): CRUD Loja

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,22 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { PrismaModule } from './prisma/prisma.module';
+import { AuthModule } from './auth/auth.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
+import { LojaModule } from './loja/loja.module';
 
 @Module({
-  imports: [PrismaModule, UserModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    PrismaModule,
+    UserModule,
+    AuthModule,
+    LojaModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
